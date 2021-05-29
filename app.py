bfdb2f7b258b36209e9510b88b52783ad1399c35
ONKAR <ONKAR@LAPTOP-A7JJJRQF>
2021-05-29 10:50:01 +0530

    Email is added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,IntegerField,BooleanField
-from wtforms.validators import InputRequired,Length, AnyOf
+from wtforms.validators import InputRequired,Length, AnyOf, Email
 
 class LoginForm(FlaskForm):
    username = StringField('username',validators=[
@@ -14,7 +14,9 @@ class LoginForm(FlaskForm):
 
    age = IntegerField('age')
    yesno = BooleanField('yesno')
+   Email = StringField('email',validators=[
 
+   ])
 
 def create_app():
    app = Flask(__name__)
@@ -25,7 +27,7 @@ def create_app():
       form = LoginForm()
 
       if form.validate_on_submit():
-         return f'<h1>Username: {form.username.data} Password: {form.password.data} Age:{form.age.data} Yesno:{form.yesno.data}</h1>'
+         return f'<h1>Username: {form.username.data} Password: {form.password.data} Age:{form.age.data} Yesno:{form.yesno.data} Email:{form.email.data}</h1>'
 
       return render_template('index.html',form=form)
 
